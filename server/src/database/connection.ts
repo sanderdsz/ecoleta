bcfb8c44ac07b4717517dd5907113e37307074fe
sander <sanderdsz@gmail.com>
2020-06-03 20:47:32 -0300

    migrations and tables creation finished

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,6 +1,11 @@
 import knex from "knex";
 import path from "path";
 
+/**
+ * Utilizar o comando npm run knex:migrate
+ * para realizar as alterações em banco
+ */
+
 const connection = knex({
   client: "sqlite3",
   connection: {
