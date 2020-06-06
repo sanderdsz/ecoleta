821d377ad7bcbbfd13b2c92cc797164cf34cbee4
sander <sanderdsz@gmail.com>
2020-06-06 11:32:33 -0300

    project done

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -5,6 +5,7 @@ import api from "../../services/api";
 import axios from "axios";
 import { LeafletMouseEvent } from "leaflet";
 
+import Dropzone from "../../components/Dropzone";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 import { FiArrowLeft } from "react-icons/fi";
@@ -50,6 +51,9 @@ const CreatePoint = () => {
   });
   // Armazena os itens selecionados
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  // Armazena imagem da dropzone
+  const [selectedFile, setSelectedFile] = useState<File>();
+
   // Retorna usuário à página principal
   const history = useHistory();
 
@@ -146,16 +150,20 @@ const CreatePoint = () => {
     const city = selectedCity;
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+
+    const data = new FormData();
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
+
     await api.post("points", data);
     alert("Pick-up spot registred");
     history.push("/");
@@ -173,6 +181,8 @@ const CreatePoint = () => {
 
       <form onSubmit={handleSubmit}>
         <h1>Pick-up spot registration</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
